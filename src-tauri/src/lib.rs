c605d6c8b2b6c66c270e609cafdebc98e73a26da
Tim <mr.tim9191@gmail.com>
2025-07-04 23:48:10 +0200

    Add tauri-plugin-updater for automatic updates and enhance configuration

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -107,6 +107,7 @@ async fn take_sip(
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     let app = tauri::Builder::default()
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_os::init())
         .plugin(tauri_plugin_notification::init())
         .plugin(tauri_plugin_opener::init())
